9dfeba64ea9d45570cc33633fb9b02ffda94e81d
Tanner Klipstein <64111041+TTkindboy@users.noreply.github.com>
2026-04-26 00:24:18 +0000

    Update temp dir discovery, add notifcation support, update error handeling to use anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
+use anyhow::Ok;
 use arboard::Clipboard;
 use simplelog::*;
 use std::fs::File;
+use notify_rust::Notification;
 
-fn main() {
-    let log_file = if cfg!(target_os = "macos") {
-        "/tmp/blipclip.log"
-    } else {
-        "C:/temp/blipclip.log"
-    };
+fn main() -> anyhow::Result<()> {
+    let log_path = std::env::temp_dir().join("blipclip.log");
     WriteLogger::init(
         LevelFilter::Debug,
         Config::default(),
-        File::create(log_file).unwrap(),
-    ).unwrap();
-    let mut clipboard = Clipboard::new().unwrap();
+        File::create(&log_path)?,
+    )?;
+    let mut clipboard = Clipboard::new()?;
 
     log::info!("app started");
     log::info!("clipboard text: {}", clipboard.get_text().unwrap())
-    
+    Notification::new()
+    .summary("Blipclip")
+    .body("Clipboard text copied to log file!")
+    .show()?;
+    Ok(())
 }
