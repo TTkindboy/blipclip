a2548e547b094498669a3632ca2bc4abcfa7af32
Tanner Klipstein <64111041+TTkindboy@users.noreply.github.com>
2026-05-03 18:05:58 +0000

    Handle blipc files recieving side

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
-use anyhow::Ok;
+use anyhow::{Context, Ok};
 use arboard::Clipboard;
 use simplelog::*;
-use std::fs::File;
-use notify_rust::Notification;
+use std::{fs, fs::File};
 
 fn main() -> anyhow::Result<()> {
+    init_logger()?;
+
+    if let Err(error) = run() {
+        log::error!("{error:#}");
+        return Err(error);
+    }
+
+    Ok(())
+}
+
+fn init_logger() -> anyhow::Result<()> {
     let log_path = std::env::temp_dir().join("blipclip.log");
     WriteLogger::init(
         LevelFilter::Debug,
         Config::default(),
         File::create(&log_path)?,
     )?;
-    let mut clipboard = Clipboard::new()?;
 
-    log::info!("app started");
-    log::info!("clipboard text: {}", clipboard.get_text().unwrap());
-    Notification::new()
-    .summary("Blipclip")
-    .body("Clipboard text copied to log file!")
-    .show()?;
+    Ok(())
+}
+
+fn run() -> anyhow::Result<()> {
+    let file_path = std::env::args_os()
+        .nth(1)
+        .context("no blipc file provided")?;
+    log::info!("opening {}", file_path.to_string_lossy());
+
+    let contents = fs::read_to_string(&file_path)
+        .with_context(|| format!("failed to read {}", file_path.to_string_lossy()))?;
+    log::info!("read {} bytes", contents.len());
+
+    Clipboard::new()?
+        .set_text(contents)
+        .context("failed to copy file contents to clipboard")?;
+    log::info!("copied file contents to clipboard");
+
+    fs::remove_file(&file_path)
+        .with_context(|| format!("failed to delete {}", file_path.to_string_lossy()))?;
+    log::info!("deleted {}", file_path.to_string_lossy());
+
     Ok(())
 }
